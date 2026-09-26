edd8f26d0d5c470df876c084ebd1b412df9a33f1
paulvincent6 <paul.contrerassss@gmail.com>
2026-09-27 01:35:20 +0800

    register sprin done

--- ContrerasLE2/BlogTestUI/Program.cs
+++ ContrerasLE2/BlogTestUI/Program.cs
@@ -1,7 +1,7 @@
-﻿using BlogDataLibrary;
+﻿using BlogDataLibrary.Data;
 using BlogDataLibrary.Database;
+using BlogDataLibrary.Models;
 using Microsoft.Extensions.Configuration;
-using System;
 using System.IO;
 
 namespace BlogTestUI
@@ -10,35 +10,72 @@ namespace BlogTestUI
     {
         static void Main(string[] args)
         {
-            // Load appsettings.json
-            IConfiguration config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
+            SqlData db = GetConnection();
 
-            // Create the database connection service
-            ISqlDataAccess db = new SqlDataAccess(config);
+            Register(db);
 
-            // Inject the database service into PostData
-            PostData postData = new PostData(db);
+            Console.WriteLine("Register Successfuly!.\n");
 
-            Console.WriteLine("Blog Test UI");
-            Console.WriteLine("------------");
-
-            // Get all posts
-            var posts = postData.GetAllPosts();
-
-            foreach (var post in posts)
-            {
-                Console.WriteLine($"ID: {post.Id}");
-                Console.WriteLine($"Title: {post.Title}");
-                Console.WriteLine($"Author: {post.UserName}");
-                Console.WriteLine($"Body: {post.Body}");
-                Console.WriteLine($"Date: {post.DateCreated}");
-                Console.WriteLine("----------------------------");
-            }
-
+            Console.WriteLine("Press Enter to exit...");
             Console.ReadLine();
+        }
+
+        static SqlData GetConnection()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration config = builder.Build();
+
+            ISqlDataAccess dbAccess = new SqlDataAccess(config);
+            SqlData db = new SqlData(dbAccess);
+
+            return db;
+        }
+
+        private static UserModel GetCurrentUser(SqlData db)
+        {
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            UserModel user = db.Authenticate(username, password);
+
+            return user;
+        }
+
+        public static void Authenticate(SqlData db)
+        {
+            UserModel user = GetCurrentUser(db);
+
+            if (user == null)
+            {
+                Console.WriteLine("Invalid credentials.");
+            }
+            else
+            {
+                Console.WriteLine($"Welcome, {user.UserName}");
+            }
+        }
+
+        public static void Register(SqlData db)
+        {
+            Console.Write("Enter new username: ");
+            var username = Console.ReadLine();
+
+            Console.Write("Enter new password: ");
+            var password = Console.ReadLine();
+
+            Console.Write("Enter first name: ");
+            var firstName = Console.ReadLine();
+
+            Console.Write("Enter last name: ");
+            var lastName = Console.ReadLine();
+
+            db.Register(username, firstName, lastName, password);
         }
     }
 }
